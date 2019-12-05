b058d76546f3b074aaa4bce3cf80e9f0d111ad45
Gustavo Flores <37113826+Gussiny@users.noreply.github.com>
2019-12-04 21:23:39 -0600

    Conexion php

---
+++ Conn.php
@@ -0,0 +1,32 @@
+<?php
+
+//  DATOS DEL SERVIDOR
+$servername = "localhost";
+$username = "facilit1_admin";
+$password = "ALPQD3CbBmtUzjV";
+
+//  CONEXION AL SERVIDOR
+$conn = new mysqli($servername, $username, $password);
+
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}else{
+    echo "Conexión exitosa";
+    mysqli_select_db($conn, "facilit1_axios_dev_db");
+    if ($result = mysqli_query($conn, "SELECT DATABASE()")) {
+        $row = mysqli_fetch_row($result);
+        printf("Default database is %s.\n", $row[0]);
+        mysqli_free_result($result);
+    }else{
+        echo "Error al conectarse a la base de datos";
+    }
+}
+
+
+if ($result = $mysqli->query("SELECT * FROM Alumno")) {
+    printf("Select returned %d rows.\n", $result->num_rows);
+
+    /* free result set */
+    $result->close();
+}
+?>
