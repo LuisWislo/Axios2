8fa67dd36cfe77ec79081f702fd6d2a8e5e5b792
Alberto Labrada <31485362+betolabrada@users.noreply.github.com>
2020-01-16 09:38:00 -0600

    test database in xampp

--- config/Conn.php
+++ config/Conn.php
@@ -1,9 +1,16 @@
 <?php
 
-//  DATOS DEL SERVIDOR
+// //  DATOS DEL SERVIDOR
+// $servername = "localhost";
+// $username = "facilit1_admin";
+// $password = "ALPQD3CbBmtUzjV";
+// $database = "facilit1_axios_dev_db";
+
+// PRUEBAS EN XAMPP
 $servername = "localhost";
-$username = "facilit1_admin";
-$password = "ALPQD3CbBmtUzjV";
+$username = "root";
+$password = "";
+$database = "facilit1_axios_dev_db_test";
 
 //  CONEXION AL SERVIDOR
 $conn = new mysqli($servername, $username, $password);
@@ -12,7 +19,7 @@ if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
 }else{
     //echo("Conexión exitosa <br>");
-    mysqli_select_db($conn, "facilit1_axios_dev_db");
+    mysqli_select_db($conn, $database);
     if ($result = mysqli_query($conn, "SELECT DATABASE()")) {
         $row = mysqli_fetch_row($result);
         //printf("Default database is %s.\n", $row[0]);
